6ff918d4e5af6454e732a7395b929db59b9235c3
shreesha <shreevinayaka977@gmail.com>
2024-07-16 02:07:03 +0530

    Phase 2- part d done

--- app.js
+++ app.js
@@ -7,10 +7,17 @@ const ejsmate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError.js');
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-const listings =require("./routes/listing.js");
-const reviews =require("./routes/review.js");
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js');
+
+
+const listingRouter =require("./routes/listing.js");
+const reviewRouter =require("./routes/review.js");
+const userRouter = require('./routes/user.js');
+
 
 const sessionOptions={
     secret:"mysupersecretcode",
@@ -31,6 +38,13 @@ app.get('/', (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "views"))
@@ -60,8 +74,17 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use("/listings",listings)
-app.use("/listings/:id/reviews",reviews)
+// app.get("/demouser",async (req, res) => {
+//     let fakeuser =new User({
+//        email:"user@example.com",
+//        username:"user",
+//     });
+//     let registeredUSer =await User.register(fakeuser,"helloworld");
+//     res.send(registeredUSer)
+// })
+app.use("/listings",listingRouter)
+app.use("/listings/:id/reviews",reviewRouter)
+app.use("/",userRouter);
 
 
 // app.get('/testlisting',async (req,res)=>{
