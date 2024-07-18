97e083194cf9cad9de778320c8da2895ef8de43e
shreesha <shreevinayaka977@gmail.com>
2024-07-18 22:03:18 +0530

    Part b done

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,13 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: [77.2090, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    style:'mapbox://styles/mapbox/streets-v11', // style URL
     zoom: 9 // starting zoom
 });
-console.log(coordinates);
 
-// const marker = new mapboxgl.Marker()
-//         .setLngLat(coordinates)//listing.geometry.coordinates
-//         .addTo(map)
+const marker = new mapboxgl.Marker({color:'red'})
+        .setLngLat(listing.geometry.coordinates)//listing.geometry.coordinates
+        .setPopup(new mapboxgl.Popup({offset: 25})
+        .setHTML(`<h5>${listing.title}</h5><p>Exact Location will be provided after Booking</p>`))
+        .addTo(map);
