38a87e6e8f5e82433afe8b7302f028e600d26dd2
shreesha <shreevinayaka977@gmail.com>
2024-07-19 14:49:00 +0530

    Last Phase

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ const path = require("path");
 const methodOveride = require('method-override');
 const ejsmate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError.js');
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
+
 
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
@@ -22,9 +24,20 @@ const listingRouter =require("./routes/listing.js");
 const reviewRouter =require("./routes/review.js");
 const userRouter = require('./routes/user.js');
 
+const store = MongoStore.create({
+    mongoUrl:dbUrl,
+    crypto:{
+        secret:process.env.SECRET
+    },
+    touchAfter:24*60*60
+})
 
+store.on("error",()=>{
+    console.log("Error in mongo session store",err)
+})
 const sessionOptions={
-    secret:"mysupersecretcode",
+    store:store,
+    secret:process.env.SECRET,
     resave:false,
     saveUninitialized:true,
     cookie:{
@@ -39,6 +52,7 @@ const sessionOptions={
 // app.get('/', (req, res) => {
 //     res.send("hi,I am root");
 // });
+
 
 app.use(session(sessionOptions));
 app.use(flash())
@@ -61,7 +75,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 main().
     then(() => {
